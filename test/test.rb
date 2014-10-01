848460d5d16d53af54d9c6f9510f9bf4b86d10fc
Debora MPB <deborampb@gmail.com>
2014-10-01 11:18:57 +0100

    Creando los test

--- test/test.rb
+++ test/test.rb
@@ -2,7 +2,7 @@ ENV['RACK_ENV'] = 'test'
 require 'test/unit'
 require 'minitest/autorun'
 require 'rack/test'
-require_relative '../twitter.rb'
+require_relative '../sinatra.rb'
 
 include Rack::Test::Methods
 
@@ -12,8 +12,24 @@ end
 
 decribe "Test twitter.rb" do
 	before :all do
-		@usuario = "DeboraMpb"
+		@twitt = FriendsPopular.new
+		@client = my_twitter_client()
+		@u1 = "DeboraMpb"
+		@u2 = "pepitopepe"
 end
+
+if "El nombre de usuario debe ser correcto" do
+	assert @twitt.friends(@cliente, @u1)
+end
+
+if "El número de usuarios a consultar debe ser menor o igual que 10" do
+	assert_equal 10, @twitt.friends(@client,@u1)
+end
+
+if "El usuario no tiene amigos" do
+	assert @twitt.friends(@client,@u1)
+end
+
 	
 
 end
