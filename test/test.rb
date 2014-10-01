b5bc2a8f82b71b2a28694a6684fb1ca7d6c67da5
Debora MPB <deborampb@gmail.com>
2014-10-01 09:59:37 +0100

    creando fichero de test

--- test/test.rb
+++ test/test.rb
@@ -1,11 +1,19 @@
 ENV['RACK_ENV'] = 'test'
-require 'twitter.rb'
 require 'test/unit'
 require 'minitest/autorun'
 require 'rack/test'
+require_relative '../twitter.rb'
 
 include Rack::Test::Methods
 
 def app
   Sinatra::Application
+end
+
+decribe "Test twitter.rb" do
+	before :all do
+		@usuario = "DeboraMpb"
+end
+	
+
 end
