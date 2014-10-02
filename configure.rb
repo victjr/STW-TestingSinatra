90d4c8f0168ac4245cc12da26b0a22339a5e4929
Victor Juidias Rodriguez <alu0100537017@ull.edu.es>
2014-10-03 00:01:56 +0100

    index.html

--- configure.rb
+++
@@ -1,8 +0,0 @@
-def my_twitter_client
-  Twitter::REST::Client.new do |config|
-    config.consumer_key        = "DKXw2l2rHwvYYEPNGhgrFbrEI"
-    config.consumer_secret     = "En9FKjYZzyMjqnFWG7tntM418uiZMxJyzh7OJ1JZ2LDmGvqA31"
-    config.access_token        = "561931192-MSNIFvD2QXhnfH8pv8HwdqXjFhZuIMg4y54tfnLO"
-    config.access_token_secret = "fVIjFNLQRWk8VZhBP61IvP2DNHstaaOy7G2e0qIaGVyvg"
-  end
-end
